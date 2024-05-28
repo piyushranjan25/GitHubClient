b504e140c2ef7e538d61ccd69e0ff743d237fd67
Piyush_Ranjan <96933385+piyushranjan25@users.noreply.github.com>
2024-05-28 15:20:53 +0530

    Update utils.component.spec.ts

--- src/app/components/shared/utils/utils.component.spec.ts
+++ src/app/components/shared/utils/utils.component.spec.ts
@@ -1,15 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UtilsComponent } from './utils.component';
 
 describe('UtilsComponent', () => {
   let component: UtilsComponent;
   let fixture: ComponentFixture<UtilsComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UtilsComponent ]
+    })
+    .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UtilsComponent]
-    });
     fixture = TestBed.createComponent(UtilsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +21,27 @@ describe('UtilsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have initial values', () => {
+    expect(component.showLoading).toBe(true);
+    expect(component.userToken).toBe('');
+    expect(component.itemsPerPage).toBe(10);
+    expect(component.dataTouched).toBe(false);
+  });
+
+  it('should return an empty string for getUserToken', () => {
+    expect(component.getUserToken()).toBe('');
+  });
+
+  it('should set showLoading to true when startLoader is called', () => {
+    component.startLoader();
+    expect(component.showLoading).toBe(true);
+  });
+
+  it('should set showLoading to false when stopLoader is called', () => {
+    component.stopLoader();
+    expect(component.showLoading).toBe(false);
+  });
 });
+
+
